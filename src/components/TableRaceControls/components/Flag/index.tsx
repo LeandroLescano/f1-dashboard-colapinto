fcc7f30c9db277b7284f69a1c9c5e5c111b54e70
FJanza <franciscojanza001@gmail.com>
2024-11-28 18:25:37 -0300

    add Falg component and svgs

---
+++ src/components/TableRaceControls/components/Flag/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {FlagProps} from "./types";
+const flags = {
+  GREEN: "/images/svg/flags/green-flag.svg",
+  CLEAR: "/images/svg/flags/green-flag.svg",
+  YELLOW: "/images/svg/flags/yellow-flag.svg",
+  BLUE: "/images/svg/flags/blue-flag.svg",
+  "DOUBLE YELLOW": "/images/svg/flags/duoble-yellow-flag.svg",
+  CHEQUERED: "/images/svg/flags/chequered.svg",
+  RED: "/images/svg/flags/red-flag.svg",
+};
+const Flag = ({type, width = 20, height = 20}: FlagProps) => {
+  return <img src={flags[type]} style={{width: width, height: height}} />;
+};
+
+export default Flag;
