d522ac8c0e561e01c7937630d8a322834527efaf
Marcos Monaco <114680984+marcosmonaco@users.noreply.github.com>
2024-11-27 21:47:05 -0300

    DASHCOL-8: Add formula 1 font (#9)
    * add Leaderboard components, add all  manufacturers SVG Logos, add colors to color palette
    
    * Visual fix to ferrario logo
    
    * add formula1 font, visual modifications according to the new font
    
    * add Leaderboard components, add all  manufacturers SVG Logos, add colors to color palette
    
    * add formula1 font, visual modifications according to the new font

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -16,14 +16,14 @@ export default function Leaderboard() {
       duration_sector_2: 32.843,
       duration_sector_3: 36.953,
       segments_sector_1: [null, 2048, 2048, 2048, 2048, 2048],
-      segments_sector_2: [2048, 2048, 2048, 2048, 2048, 2048, 2051],
+      segments_sector_2: [2048, 2048, 2048, 2048, 2048, 2048, 2048],
       segments_sector_3: [2048, 2048, 2048, 2048, 2048, 2048, 2049, 2049],
       lap_number: 25,
     },
   ];
 
   return (
-    <div className="max-w-[1100px]">
+    <div className="max-w-[1100px] font-formula">
       <div className="flex flex-col gap-4 text-white bg-f1-black-200 mx-auto p-1 rounded-xl">
         <div className="flex flex-col gap-1 bg-f1-black-300 rounded-lg border-2 border-f1-gray-300/50">
           <div className="flex flex-col sm:flex-row items-center justify-between p-4 gap-4">
@@ -41,12 +41,12 @@ export default function Leaderboard() {
               {/* TODO Pasar GP dinamico */}
               <p className="text-xl">Grand Prix Las Vegas</p>
               {/* TODO Pasar vueltas dinamicas */}
-              <p className="text-xl">8/50</p>
+              <p className="text-xl">Vuelta 8 / 50</p>
             </div>
           </div>
           <div className="overflow-x-auto">
             <div className="min-w-[900px]">
-              <div className="grid grid-cols-8 gap-4 px-2 py-2 whitespace-nowrap border-y border-f1-gray-300 bg-gradient-to-t from-f1-gray-400 to-f1-black-400 text-center text-sm font-semibold">
+              <div className="grid grid-cols-8 gap-4 px-2 py-2 whitespace-nowrap border-y border-f1-gray-300 bg-gradient-to-t from-f1-gray-400 to-f1-black-400 text-center text-sm">
                 <div>POS</div>
                 <div>Conductor</div>
                 <div>Tiempo de vuelta</div>
@@ -60,12 +60,12 @@ export default function Leaderboard() {
                 {lap8.map((lap, index) => (
                   <div
                     key={lap.driver_number}
-                    className="grid grid-cols-8 gap-4 items-center font-semibold text-sm last:border-b-0"
+                    className="grid grid-cols-8 gap-4 items-center text-sm last:border-b-0"
                   >
                     <div className="text-center border-r border-f1-gray-300/20 py-2">
                       {index + 1}
                     </div>
-                    <div className="text-center flex flex-row gap-2">
+                    <div className="text-center flex flex-row gap-2 justify-center">
                       {/* TODO Pasar Logo dinamico */}
                       <img
                         src="/images/svg/manufacturers/mercedes.svg"
