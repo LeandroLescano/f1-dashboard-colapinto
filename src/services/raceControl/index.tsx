a7d7f07afcaf95d6ea957020e3d6dd04c6d8060e
Janza <franciscojanza001@gmail.com>
2024-11-28 15:21:27 -0300

    Racecontrols services (#14)
    * add service of raceControls
    
    * refactor of types

---
+++ src/services/raceControl/index.tsx
@@ -0,0 +1,21 @@
+import {f1Client} from "@services/axios";
+import {toCamel} from "@utils/object";
+
+import {RaceControl, RaceControlDTO} from "./types";
+
+/**
+ * Returns a list of race control
+ * @param {number | "latest"} sessionKey ID of the Session
+ * @returns {RaceControl[]} array of raceControl
+ */
+export const getRaceControls = async (
+  sessionKey?: number | "latest"
+): Promise<RaceControl[]> => {
+  const {data} = await f1Client.get<RaceControlDTO[]>("/race_control", {
+    params: {
+      session_key: sessionKey,
+    },
+  });
+
+  return toCamel(data.reverse()) as RaceControl[];
+};
--- src/services/raceControl/types.d.ts
+++ src/services/raceControl/types.d.ts
@@ -1,3 +1,18 @@
+import {CamelizeKeys} from "@interfaces/generic";
+
+export interface RaceControlDTO {
+  category: string;
+  date: Date;
+  driver_number: number;
+  flag: string;
+  lap_number: number;
+  meeting_key: number;
+  message: string;
+  scope: string;
+  sector: null;
+  session_key: number;
+}
+
 export type Flag =
   | "GREEN"
   | "YELLOW"
@@ -5,3 +20,5 @@ export type Flag =
   | "DOUBLE YELLOW"
   | "CHEQUERED"
   | "RED";
+
+export type RaceControl = CamelizeKeys<RaceControlDTO>;
